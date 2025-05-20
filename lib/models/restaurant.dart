0b4d158e736c64d1ed738ca7ca6f3d54597b7d6d
Roger Fan <sweric2u@gmail.com>
2025-05-20 22:42:10 +0800

    fix: enum

--- lib/models/restaurant.dart
+++ lib/models/restaurant.dart
@@ -1,6 +1,6 @@
 import 'package:foodie/enums/genre_tag.dart';
 import 'package:foodie/enums/halal_tag.dart';
-import 'package:foodie/enums/vegen_tag.dart';
+import 'package:foodie/enums/vegan_tag.dart';
 import 'package:foodie/models/menu.dart';
 import 'package:foodie/models/restaurant_info.dart';
 import 'package:foodie/models/restaurant_review.dart';
@@ -9,8 +9,8 @@ class Restaurant {
   String? name, summary;
   int? rating;
   List<GenreTag>? genreTags;
-  List<HalalTag>? halalTags;
-  List<VegenTag>? vegenTags;
+  HalalTag? halalTags;
+  VeganTag? vegenTags;
   List<RestaurantReview>? reviews;
   Menu? menu;
   RestaurantInfo? info;
