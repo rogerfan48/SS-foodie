805ed0a61d154b2c4265f814df050fd011cbd441
SeanChang-3245 <seanchangcode@gmail.com>
2025-06-08 21:42:11 +0800

    fix: restaurant review summarize argument typo

--- lib/services/summarize_restaurant_review.dart
+++ lib/services/summarize_restaurant_review.dart
@@ -1,10 +1,13 @@
 import 'package:cloud_functions/cloud_functions.dart';
 
 // Input a list of strings representing a review and return a summarized version of the review.
-Future<String> summarizeRestaurantReview(String dishName, List<String> reviewTexts) async {
+Future<String> summarizeRestaurantReview(String restaurantId, List<String> reviews) async {
   try {
-    final HttpsCallable callable = FirebaseFunctions.instance.httpsCallable('summarizeReview');
-    final response = await callable.call(reviewTexts);
+    final HttpsCallable callable = FirebaseFunctions.instance.httpsCallable('summarizeRestaurantReview');
+    final response = await callable.call({
+      "restaurantId": restaurantId,
+      "reviews": reviews,
+    });
     final data = response.data as String;
     return data;
   } catch (e) {
